a57525517327e88c0882bdbefd12ff3c70643d13
mayur <mayurramavat@gmail.com>
2013-02-22 01:37:48 +0530

    phase 1 completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,15 @@
 class TasksController < ApplicationController
+  
   def index
     @tasks = Task.paginate(page: params[:page])
   end
-  
+
+
   def new
     @task = Task.new
   end
-  
+
+
   def create
     @task = Task.create(params[:task])
     if @task.save
@@ -15,8 +18,33 @@ class TasksController < ApplicationController
       render 'new'
     end
   end
-  
+
+
   def show
     @task = Task.find(params[:id])
   end
+
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(params[:task])
+      flash[:success] = "Task Upadated"
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    Task.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to tasks_url
+  end
+
 end
